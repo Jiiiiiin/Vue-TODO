743f20fdebe72bc58b687641241417c35187f522
zhaojin <jiiiiiin@yeah.net>
2018-07-09 09:45:25 +0800

    refactor: 添加针对图片和stylus的处理规则

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './asset/styles/test.css'
+import './asset/styles/test-stylus.styl'
+import './asset/images/favicon.jpg'
 
 const root = document.createElement('div')
 document.body.appendChild(root)
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,31 @@ module.exports = {
     rules: [{
       test: /\.vue$/,
       loader: 'vue-loader'
+    }, {
+      test: /\.css$/,
+      use: [
+        // style-loader 会将css-loader处理完毕的css，解析到webpack生成的bundle中，以js的形式写到html的style标签中
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.styl$/,
+      use: [
+        // style-loader 会将css-loader处理完毕的css，解析到webpack生成的bundle中，以js的形式写到html的style标签中
+        'style-loader',
+        'css-loader',
+        'stylus-loader'
+      ]
+    }, {
+      test: /\.(jpg|png)$/,
+      use: [{
+        // url-loader 允许你有条件地将文件转换为内联的 base-64 URL (当文件小于给定的阈值)，这会减少小文件的 HTTP 请求数。如果文件大于该阈值，会自动的交给 file-loader 处理。
+        loader: 'url-loader',
+        options: {
+          limit: 1024,
+          name: '[name]-urlloader.[ext]'
+        }
+      }]
     }]
   },
   plugins: [
