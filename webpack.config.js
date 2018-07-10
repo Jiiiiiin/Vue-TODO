9c244fe01ccafe1746e8be6dd606200008b91b1b
zhaojin <jiiiiiin@yeah.net>
2018-07-10 15:40:12 +0800

    feat: 针对生产版本完成，提取css等优化

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 // https://webpack.docschina.org/plugins/html-webpack-plugin/
 const HTMLPlugin = require('html-webpack-plugin')
+// 期望把css分离出打包之后的bundle文件
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const webpack = require('webpack')
 // 通过package script中设置环境变量
 const isDev = process.env.NODE_ENV == 'development'
@@ -13,7 +16,7 @@ const config = {
   output: {
     // https://juejin.im/post/5ae9ae5e518825672f19b094
     // publicPath: '/pmobile/',
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'dist')
   },
   module: {
@@ -27,37 +30,15 @@ const config = {
         // https://vue-loader.vuejs.org/zh/guide/pre-processors.html#typescript
         test: /\.pug$/,
         loader: 'pug-plain-loader'
-      }, {
-        test: /\.css$/,
-        use: [
-          // style-loader 会将css-loader处理完毕的css，解析到webpack生成的bundle中，以js的形式写到html的style标签中
-          'style-loader',
-          'css-loader'
-        ]
       },
-      {
-        test: /\.styl(us)?$/,
-        use: [
-          // https://segmentfault.com/q/1010000004579566
-          // style-loader 会将css-loader处理完毕的css，解析到webpack生成的bundle中，以js的形式写到html的style标签中
-          'style-loader',
-          // css-loader 是处理css文件中的url()等
-          'css-loader',
-          {
-            // https://www.ibm.com/developerworks/cn/web/1604-postcss-css/index.html
-            // 使用postcss，具体配置在postcss.config.js中
-            // PostCSS 的主要功能只有两个：第一个就是前面提到的把 CSS 解析成 JavaScript 可以操作的 AST，第二个就是调用插件来处理 AST 并得到结果。
-            // Autoprefixer: https://www.ibm.com/developerworks/cn/web/1604-postcss-css/index.html
-            loader: 'postcss-loader',
-            options: {
-              // 直接使用stylus-loader生成的css，提高编译速度
-              sourceMap: true
-            }
-          },
-          // 是将stylus文件编译成css
-          'stylus-loader'
-        ]
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     // style-loader 会将css-loader处理完毕的css，解析到webpack生成的bundle中，以js的形式写到html的style标签中
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
+      // },
       {
         test: /\.(jpg|png|svg)$/,
         use: [{
@@ -89,6 +70,29 @@ const config = {
 
 // 根据不同环境去做判断不同的配置
 if (isDev) {
+  config.module.rules.push({
+    test: /\.styl(us)?$/,
+    use: [
+      // https://segmentfault.com/q/1010000004579566
+      // style-loader 会将css-loader处理完毕的css，解析到webpack生成的bundle中，以js的形式写到html的style标签中
+      'style-loader',
+      // css-loader 是处理css文件中的url()等
+      'css-loader',
+      {
+        // https://www.ibm.com/developerworks/cn/web/1604-postcss-css/index.html
+        // 使用postcss，具体配置在postcss.config.js中
+        // PostCSS 的主要功能只有两个：第一个就是前面提到的把 CSS 解析成 JavaScript 可以操作的 AST，第二个就是调用插件来处理 AST 并得到结果。
+        // Autoprefixer: https://www.ibm.com/developerworks/cn/web/1604-postcss-css/index.html
+        loader: 'postcss-loader',
+        options: {
+          // 直接使用stylus-loader生成的css，提高编译速度
+          sourceMap: true
+        }
+      },
+      // 是将stylus文件编译成css
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#inline-source-map'
   config.devServer = {
     port: '8000',
@@ -103,6 +107,36 @@ if (isDev) {
     // 启用模块热替换(Enable Hot Module Replacement - HMR)
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
+  )
+} else {
+  config.output.filename = '[name].[chunkhash].bundle.js'
+  // 提取css
+  config.module.rules.push({
+    test: /\.styl(us)?$/,
+    use: ExtractTextPlugin.extract({
+      // 将生成的css文件，写入html
+      fallback: 'style-loader',
+      // 处理生成css
+      use: [
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true
+          }
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  // 指定输出的css的名称
+  // 使用css文件内容的hash
+  config.plugins.push(
+    new CleanWebpackPlugin(['dist'], {
+      root: path.join(__dirname, './'),
+      verbose: true
+    }),
+    new ExtractTextPlugin('styles.[contentHash:8].css')
   )
 }
 
